2d44a95afbe38ecc4407c738f70392938d726ded
Helge <helwes04@edu.umea.se>
2024-07-07 21:58:53 +0200

    inte fungerande del
    påbörjat pie chart i Chart.js, ger error, chart.js och react-chartjs-2 tillagda i package.json, allt annat funkar

--- frontend2/src/components/Chart.js
+++ frontend2/src/components/Chart.js
@@ -1,12 +1,31 @@
+// src/components/Chart.js
 import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Chart = () => {
-  return (
-      <div>
-          <h2>Chart</h2>
-          <h2>Test</h2>
-      </div>
-  );
+    const data = {
+        labels: ['basket', 'rehab', 'gym'],
+        datasets: [
+            {
+                data: [2, 1, 1],
+                backgroundColor: ['#F88D2B', '#AA82FF', '#73A8F6'],
+            }
+        ]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+    };
+
+    return (
+        <div style={{ width: '400px', height: '400px' }}>
+            <Pie data={data} options={options} />
+        </div>
+    );
 };
 
 export default Chart;
