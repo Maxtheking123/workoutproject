fa6f1c6aa36f4ce9099320c774dd37e83ae026e1
Maxtheking123 <lindholm.maximilian@gmail.com>
2024-07-08 18:41:13 +0200

    chart borde va klart, inte testat nog men endå

--- frontend2/src/components/Chart.js
+++ frontend2/src/components/Chart.js
@@ -3,6 +3,7 @@ import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { AuthContext } from "../context/AuthContext";
 import '../css/Chart.css';
+import backIcon from "../images/backArrow.svg";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -11,6 +12,8 @@ const Chart = () => {
     const { fetchCalendarEntries, fetchCategoryEntries } = useContext(AuthContext);
     // har lärt mig att det är typ som en global variabel
     const [chartData, setChartData] = useState(null);
+    const [chartPercentages, setChartPercentages] = useState(null);
+    const [timeSinceLast, setTimeSinceLast] = useState(null);
 
     const getEntries = async () => {
         // hämtar data ifrån fetchCalendarEntries och fetchCategoryEntries, du kan kolla i inspect console och se hur datan ser ut
@@ -19,9 +22,24 @@ const Chart = () => {
         console.log('fetchedEntries:', fetchedEntries);
         console.log('fetchedCategories:', fetchedCategories);
 
+        var timeFrame = 7;
+        const timeElement = document.getElementsByClassName('timeButton selected')[0];
+        if (timeElement.id === 'lastMonth') {
+            timeFrame = 30;
+        }
+        if (timeElement.id === 'lastWeek') {
+            timeFrame = 7;
+        }
+        if (timeElement.id === 'nextWeek') {
+            timeFrame = -7;
+        }
+
         // kollar vilka kategorier som används och sparar deras id
         const usedCategoryIds = []
         for (const entry of fetchedEntries) {
+            if (new Date(entry.date) > new Date(new Date().getTime() + timeFrame * 24 * 60 * 60 * 1000)) {
+                continue;
+            }
             if (!usedCategoryIds.includes(entry.category)) {
                 usedCategoryIds.push(entry.category);
             }
@@ -61,6 +79,8 @@ const Chart = () => {
         }
         console.log('categoryColors:', categoryColors);
 
+
+
         // sätter ihop allt till data
         const data = {
             labels: categoryLabels,
@@ -73,7 +93,44 @@ const Chart = () => {
         };
         // detta ändrar den där globala variabeln högst up så det sparas typ
         setChartData(data);
+
+        // räknar ut procenten
+        const total = categoryCounts.reduce((a, b) => a + b, 0);
+        const procentages = []
+        for (const count of categoryCounts) {
+            procentages.push((count / total) * 100);
+        }
+        console.log('procentages:', procentages);
+        setChartPercentages(procentages);
+
+        // räknar ut hur länge sen varje kategori användes, omvandla till hela dagar, avrunda till hela dagar
+        const timeSinceLast = []
+        for (const category of usedCategoryIds) {
+            let time = null;
+            for (const entry of fetchedEntries) {
+                if (entry.category === category) {
+                    if (!time || entry.date > time) {
+                        time = entry.date;
+                    }
+                }
+            }
+            if (time) {
+                time = Math.round((new Date() - new Date(time)) / 1000 / 60 / 60 / 24, 0);
+            }
+            timeSinceLast.push(time);
+        }
+        console.log('timeSinceLast:', timeSinceLast);
+        setTimeSinceLast(timeSinceLast);
     };
+
+    const changeTimeFrame = (timeFrame) => {
+        const timeElements = document.getElementsByClassName('timeButton');
+        for (const timeElement of timeElements) {
+            timeElement.classList.remove('selected');
+        }
+        document.getElementById(timeFrame).classList.add('selected');
+        getEntries();
+    }
 
     // det här fick jag chatgpt hjälp med, tror den kör functionen när typ sidan har laddat klart? eller något sånt
     useEffect(() => {
@@ -84,15 +141,63 @@ const Chart = () => {
         responsive: true,
         maintainAspectRatio: false,
         borderWidth: 0,
+        plugins: {
+      legend: {
+        display: false,
+      }
+    },
     };
 
     return (
-        <div style={{ width: '400px', height: '400px' }}>
-            {chartData ? (
-                <Pie data={chartData} options={options} />
-            ) : (
-                <p>Loading...</p>
-            )}
+        <div id="chartPage">
+            <div id="topContainer">
+                <div id="homeButtonContainer">
+                    <div className="backIcon" style={{backgroundImage: `url(${backIcon})`}}></div>
+                    <div id="homeButton" onClick={() => document.location.href = '/'}>Home</div>
+                </div>
+                <div id="WorkoutsplitTitle">Workoutsplit</div>
+                <div id="timeContainer">
+                    <div id="lastMonth" className="timeButton" onClick={() => changeTimeFrame('lastMonth')}>Last month</div>
+                    <div id="lastWeek" className="timeButton selected" onClick={() => changeTimeFrame('lastWeek')}>Last week</div>
+                    <div id="nextWeek" className="timeButton" onClick={() => changeTimeFrame('nextWeek')}>Next week</div>
+                </div>
+            </div>
+            <div id="chartContainer">
+                {chartData ? (
+                    <Pie data={chartData} options={options}/>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+            <div id="leftAlignedTextContainer">
+                <div id="percentagesContainer">
+                    {chartPercentages ? (
+                        chartPercentages.map((procentage, index) => (
+                            <div key={index} className="percentage">
+                                <div className="color"
+                                     style={{backgroundColor: chartData.datasets[0].backgroundColor[index]}}></div>
+                                <div className="percentageNumber">{procentage.toFixed(0)}%</div>
+                                <div className="label">{chartData.labels[index]}</div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
+                <div id="timeSinceLastContainer">
+                    <div id="timeSinceLastTitle">Time since last workout</div>
+                    {timeSinceLast ? (
+                        timeSinceLast.map((time, index) => (
+                            <div key={index} className="timeSinceLast" style={{display: time < 0 ? 'none' : 'flex' }}>
+                                <div className="label">{chartData.labels[index]}</div>
+                                <div className="time">{time > -1 ? (time + (time === 1 ? ' day' : ' days')) : 'Never'}</div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
